c9c49d6ddd9352386404cd85222597f49cd62bec
Josh Ribakoff <josh.ribakoff@gmail.com>
2013-03-31 21:30:04 -0400

    apply PSR-2 code standards

--- modules/vaf/override/classes/Category.php
+++ modules/vaf/override/classes/Category.php
@@ -1,6 +1,6 @@
 <?php
 class Category extends CategoryCore
 {
-        
-    
+
+
 }
--- modules/vaf/vaf.php
+++ modules/vaf/vaf.php
@@ -2,9 +2,10 @@
 require_once('autoload.php');
 require_once('HelperData.php');
 
-if (!defined('_PS_VERSION_'))
-  exit;
- 
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
 class Vaf extends Module
 {
     function __construct()
@@ -25,32 +26,34 @@ class Vaf extends Module
 
     function install()
     {
-        if (parent::install() == false || !$this->registerHooks() )
+        if (parent::install() == false || !$this->registerHooks()) {
             return false;
+        }
         return true;
     }
-    
+
     function registerHooks()
     {
         return $this->registerHook('leftColumn') &&
-               $this->registerHook('displayAdminProductsExtra') &&
-               $this->registerHook('actionProductSave') &&
-               $this->registerHook('productListAssign');
+            $this->registerHook('displayAdminProductsExtra') &&
+            $this->registerHook('actionProductSave') &&
+            $this->registerHook('productListAssign');
     }
 
-    function hookDisplayAdminProductsExtra($obj){
+    function hookDisplayAdminProductsExtra($obj)
+    {
         ob_start();
         include('vafadmin.phtml');
         $content = ob_get_clean();
         return $content;
     }
 
-    function hookLeftColumn( $params )
+    function hookLeftColumn($params)
     {
         $search = new VF_FlexibleSearch(new VF_Schema, new Zend_Controller_Request_Http);
         $search->setConfig(new Zend_Config(array()));
         $search->storeFitInSession();
-        
+
         ob_start();
         include('search.phtml');
         $content = ob_get_clean();
@@ -61,23 +64,22 @@ class Vaf extends Module
     {
         return $this->hookLeftColumn($params);
     }
-    
+
     function hookActionProductSave()
     {
         $this->removeFitments();
         $this->addNewFitments();
     }
-    
+
     function hookProductListAssign($params)
     {
-        if($params['nbProducts'])
-        {
+        if ($params['nbProducts']) {
             return;
         }
         echo 'There is a conflicting module with Vehicle Fit, please contact support for instructions on fixing.';
-       // exit();
+        // exit();
     }
-  
+
     function vafProductIds()
     {
         //return '1';
@@ -90,58 +92,53 @@ class Vaf extends Module
     function flexibleSearch()
     {
         $search = new VF_FlexibleSearch(new VF_Schema, new Zend_Controller_Request_Http);
-	$search->setConfig($this->vafConfig());
+        $search->setConfig($this->vafConfig());
         return $search;
     }
-    
+
     function vafConfig()
     {
         return new Zend_Config(array());
     }
-        
+
     function removeFitments()
     {
         $request = new Zend_Controller_Request_Http;
         $schema = new VF_Schema();
-        if( is_array( $request->getParam( 'vaf-delete' ) ) && count( $request->getParam( 'vaf-delete' ) ) >= 1 )
-        {
-            foreach( $request->getParam( 'vaf-delete', array() ) as $fit )
-            {
-                $fit = explode( '-', $fit );
+        if (is_array($request->getParam('vaf-delete')) && count($request->getParam('vaf-delete')) >= 1) {
+            foreach ($request->getParam('vaf-delete', array()) as $fit) {
+                $fit = explode('-', $fit);
                 $level = $fit[0];
                 $fit = $fit[1];
-                if( $level == $schema->getLeafLevel() )
-                {
-                    Elite_Vaf_Helper_Data::getInstance()->deleteFitment($_GET['id_product'],$fit);
+                if ($level == $schema->getLeafLevel()) {
+                    Elite_Vaf_Helper_Data::getInstance()->deleteFitment($_GET['id_product'], $fit);
                 }
             }
         }
     }
 
-    
+
     function addNewFitments()
     {
         $request = new Zend_Controller_Request_Http;
-        if( is_array( $request->getParam( 'vaf' ) ) && count( $request->getParam( 'vaf' ) ) >= 1 )
-        {
-            foreach( $request->getParam( 'vaf' ) as $fit )
-            {
-                if( strpos($fit,':') && strpos($fit,';') )
-                {
+        if (is_array($request->getParam('vaf')) && count($request->getParam('vaf')) >= 1) {
+            foreach ($request->getParam('vaf') as $fit) {
+                if (strpos($fit, ':') && strpos($fit, ';')) {
                     // new logic
                     $params = explode(';', $fit);
                     $newParams = array();
-                    foreach($params as $key => $value)
-                    {
+                    foreach ($params as $key => $value) {
                         $data = explode(':', $value);
-                        if(count($data)<=1) continue;
-                         
+                        if (count($data) <= 1) {
+                            continue;
+                        }
+
                         $newParams[$data[0]] = $data[1];
                     }
-                    Elite_Vaf_Helper_Data::getInstance()->addFitment($_GET['id_product'],$newParams);
+                    Elite_Vaf_Helper_Data::getInstance()->addFitment($_GET['id_product'], $newParams);
                 }
             }
         }
     }
-    
+
 }
