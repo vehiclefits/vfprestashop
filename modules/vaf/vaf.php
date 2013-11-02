9fc80f824dcb7c76b3bf62bb19c08a8538221978
josh <josh.ribakoff@gmail.com>
2013-11-02 13:35:59 -0400

    Implemented a hack to get my module to load on every page, even if the search form isn't on that page. See the comment in the code for this hack for further justification.

--- modules/vaf/vaf.php
+++ modules/vaf/vaf.php
@@ -17,6 +17,9 @@ class Vaf extends Module
         $this->author = 'Josh Ribakoff';
         $this->need_instance = 0;
 
+        if(!isset($_SESSION)) {
+            session_start();
+        }
         parent::__construct();
 
         $this->displayName = $this->l('Vehicle Fits');
@@ -33,7 +36,28 @@ class Vaf extends Module
 
     function registerHooks()
     {
-        return $this->registerHook('leftColumn') && $this->registerHook('displayHome');
+        return $this->registerHook('leftColumn') && $this->registerHook('displayHome') && $this->registerHook('displayHeader');
+    }
+
+    function hookDisplayHeader()
+    {
+        /**
+         * This hook is not actually used.
+         * It is a hack of PrestaShops architecture to force my module to load on every page.
+         * This hook is called on every page, which is why it was choosen to be added here.
+         *
+         * The reason my module should load on every page, is the Category override is not correlated to my module.
+         * When my module adds an override for the Category class, Prestashop overrides that class, but it does not
+         * make the correlation that the overriden category class may contain code that depends on my module having loaded first.
+         *
+         * If my module is used on the "left" column, and that column is also present on the category page; this all works fine.
+         *
+         * It is when the user wants to hook my module into a section of the homepage, but still have it filter on the category page.
+         *
+         * As you can see, Prestashops architecture doesn't account for this situation, where my search form is not displayed on the page
+         * that is required to process the results (category page). Due to this, we must use some sort of hack to force my code
+         * to run on the category page - the most obvious way was to hook into some section that is on every page, such as this section - the header.
+         */
     }
 
     public function hookDisplayHome($params)
